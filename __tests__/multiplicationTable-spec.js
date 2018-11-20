27cc760d3d6ac1596fe3c9e7c750692c4fedebdb
lamve2 <venice.lam@oocl.com>
2018-11-20 23:47:32 +0800

    generateTable

--- __tests__/multiplicationTable-spec.js
+++ __tests__/multiplicationTable-spec.js
@@ -1,12 +1,20 @@
-const checkIfInputsAreValid = require('../multiplicationTable');
+const generateMultiplicationTable = require('../multiplicationTable');
 
 
 it ('should return true when a is smaller than or equal to b', () => {
-    expect(checkIfInputsAreValid (2,4)).toBe(true);
-    expect(checkIfInputsAreValid (5,3)).toBe(null);
+    expect(generateMultiplicationTable (2,4)).toBe(true);
+    expect(generateMultiplicationTable (5,3)).toBe(null);
 })
 it ('should return true when a or b is in range 1-1000', () => {
-    expect(checkIfInputsAreValid (5,10)).toBe(true);
-    expect(checkIfInputsAreValid (5,2000)).toBe(false);
+    expect(generateMultiplicationTable (5,10)).toBe(true);
+    expect(generateMultiplicationTable (5,2000)).toBe(false);
 })
+it ('should return multiplication table when a and b are valid', () => {
+    //const expected = "2*2=4\n"+
+                    // "2*3=6  3*3=9\n"+
+                    // "2*4=8  3*4=12  4*4=16";
+    expect(generateMultiplicationTable(2,4)).toBe("2*2=4\n"+
+    "2*3=6  3*3=9\n"+
+    "2*4=8  3*4=12  4*4=16");
 
+})
--- multiplicationTable.js
+++ multiplicationTable.js
@@ -8,6 +8,38 @@ function checkIfInputsAreValid(a,b) {
     }
     return true;
 }
+function formatTable (output, maxlength){
+    var formatlength = maxlength - output.length;
+    output = output + " ";
+    for (var k = 0; k < formatlength; k++){
+        output = output + " ";
+    }
+    return output;
+}
+function insertTableString(a,b,max){
+    var output = a.toString() + "*" + b.toString() + "=" + (a * b);
+    if (a == b){
+        return output;
+        }
+    return formatTable(output, (max*max).toString().length + max.toString().length + max.toString().length + 2);
 
+}
+function generateMultiplicationTable(a,b){
+    if (!checkIfInputsAreValid(a,b)){
+        return null;
+    }
+    var string = "";
+    for (var i = a; i <= b; i++){
+        for (var j = a; j <= i; j++){
+            string += insertTableString(j,i,b);
+        }
+        if (b != i) {
+            string += "\n";
+        }
 
+    }
+    return string;
+}
+
+module.exports = generateMultiplicationTable;
 module.exports = checkIfInputsAreValid;
